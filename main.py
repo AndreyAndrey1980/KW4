ac93c35f0f3741824022d64ec2e76d5433d26f97
AndreyAndrey1980 <mandibul1@mail.ru>
2024-08-08 21:42:39 +0300

    Были исправлены функции и методы по указанным ошибкам

--- main.py
+++ main.py
@@ -1,41 +1,9 @@
-from src.get_api_hh import get_api_hh
-from src.json_saver import json_saver
-from src.vacancy import vacancy
+from src.utils import user_interaction
 
-response = get_api_hh()
 
-while True:
-    user_vacancy: str = input("Text name vacancy for search:\n")
-    user_city: str = input("Text city in where you want to find vacancies:\n")
-    if isinstance(user_city, str) and isinstance(user_vacancy, str):
-        break
-    print(f"Please, text alpha, not {user_city}")
+def main():
+    user_interaction()
 
-while True:
-    user_salary = input("Text min salary for search:\n")
-    if user_salary.isdigit():
-        break
-    print(f"Please, text digit, not {user_salary}")
 
-# Get vacancies for user
-response.get_vacancy_from_api(user_vacancy)
-
-file_json = json_saver()
-
-# Save response to JSON
-file_json.save_file(response.all_vacancy)
-
-# Read JSON file
-file_vacancies = file_json.read_file()
-
-# Print vacancies for user
-vacancy = vacancy.get_vacancy_list(file_vacancies, user_city, int(user_salary))
-sorted_vacancies = sorted(vacancy)
-
-while True:
-    count = input("How vacancies do you want to see:\n")
-    if count.isdigit():
-        break
-    print(f"Please, text digit, not {count}")
-
-print(*sorted_vacancies[:int(count)])
+if __name__ == '__main__':
+    main()
--- src/vacancy.py
+++ src/vacancy.py
@@ -1,48 +1,203 @@
-from src.json_saver import json_saver
+from abc import ABC, abstractmethod
 
 
-class vacancy:
-    list_vacancies = []
+class BaseVacancy(ABC):
+    """Базовый класс для вакансии"""
 
-    def __init__(self, name_vacancy: str, salary_from: int, salary_to: int, url: str, city: str):
-        self.name_vacancy = name_vacancy
-        self.salary_from = salary_from
-        self.salary_to = salary_to
-        self.url = url
-        self.city = city
-        self.list_vacancies.append(self)
+    @abstractmethod
+    def __init__(self):
+        pass
 
-    def __repr__(self):
-        return (f"\nName of vacancy: {self.name_vacancy}\n"
-                f"Salary from: {self.salary_from}\n"
-                f"Salary to: {self.salary_to}\n"
-                f"City: {self.city}\n"
-                f"URL: {self.url}\n")
+    @abstractmethod
+    def __str__(self):
+        pass
 
+    @abstractmethod
     def __lt__(self, other):
-        if other.salary_to < self.salary_to:
-            return True
+        pass
 
-    @classmethod
-    def get_vacancy_list(cls, list_vacancy, city, salary_from) -> list:
-        """
-        Get list with vacancies dicts. This list with copy of class Vacancy
-        :return: new lisrt with copy of class Vacancy
-        """
-        for vacancy in list_vacancy:
-            name_vacancy = vacancy["name"]
-            url = vacancy["alternate_url"]
-            if vacancy["area"]["name"] == city:
-                city = vacancy["area"]["name"]
-            if vacancy["salary"] is None:
-                continue
-            elif vacancy["salary"]["to"] is not None and vacancy["salary"]["from"]:
-                if vacancy["salary"]["from"] >= salary_from:
-                    salary_from = vacancy["salary"]["from"]
-                    salary_to = vacancy["salary"]["to"]
-                    cls(name_vacancy, salary_from, salary_to, url, city)
-                else:
-                    continue
+
+class BaseVacancies(ABC):
+    """Базовый класс для списка вакансий"""
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def add_vacancy(self, new_vacancy):
+        pass
+
+    @abstractmethod
+    def delete_vacancy(self, vac_id: int):
+        pass
+
+
+class Vacancy(BaseVacancy):
+    """Класс для работы с вакансией"""
+
+    def __init__(
+        self,
+        vac_id: int,
+        name: str,
+        city: str,
+        salary_from: float,
+        salary_to: float,
+        currency: str,
+        employment: str,
+        url: str,
+    ):
+        self.id = vac_id
+        self.name: str = name
+        self.city: str = city
+        self.salary_from: float = salary_from
+        self.salary_to: float = salary_to
+        self.currency = currency
+        self.employment: str = employment
+        self.url: str = url
+
+    def __str__(self) -> str:
+        return (
+            f"id: {self.id}\n"
+            f"Название вакансии: {self.name}\n"
+            f"Город: {self.city}\n"
+            f"Зарплата\nот: {self.salary_from} {self.currency}\nдо: {self.salary_to} {self.currency}\n"
+            f"Тип занятости: {self.employment}\n"
+            f"Ссылка на вакансию: {self.url}\n"
+        )
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Vacancy):
+            raise TypeError("Вакансию можно сравнивать только с вакансией!")
+        return (self.salary_from, self.salary_to) == (other.salary_from, other.salary_to)
+
+    def __ne__(self, other) -> bool:
+        if not isinstance(other, Vacancy):
+            raise TypeError("Вакансию можно сравнивать только с вакансией!")
+        return (self.salary_from, self.salary_to) != (other.salary_from, other.salary_to)
+
+    def __lt__(self, other) -> bool:
+        if not isinstance(other, Vacancy):
+            raise TypeError("Вакансию можно сравнивать только с вакансией!")
+        return (self.salary_from, self.salary_to) < (other.salary_from, other.salary_to)
+
+    def __gt__(self, other) -> bool:
+        if not isinstance(other, Vacancy):
+            raise TypeError("Вакансию можно сравнивать только с вакансией!")
+        return (self.salary_from, self.salary_to) > (other.salary_from, other.salary_to)
+
+    def to_dict(self):
+        """Возвращает вакансию в виде словаря"""
+        return self.__dict__
+
+    @staticmethod
+    def to_list(vacancy_dict):
+        """Возвращает вакансию в виде списка"""
+        return Vacancy(
+            vacancy_dict["vacancy_title"],
+            vacancy_dict["town"],
+            vacancy_dict["salary_from"],
+            vacancy_dict["salary_to"],
+            vacancy_dict["currency"],
+            vacancy_dict["employment"],
+            vacancy_dict["url"],
+        )
+
+
+class Vacancies(BaseVacancies):
+    """Класс для работы со списком вакансий"""
+
+    def __init__(self):
+        self.__all_vacancies = []
+        self.cursor = 0
+
+    def __len__(self):
+        return len(self.all_vacancies)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.cursor >= len(self.__all_vacancies):
+            raise StopIteration
+        else:
+            result = self.__all_vacancies[self.cursor]
+            self.cursor += 1
+            return result
+
+    def __getitem__(self, index):
+        return self.__all_vacancies[index]
+
+    def __str__(self):
+        str_vac = ""
+        if len(self.__all_vacancies) > 0:
+            for vac in self.all_vacancies:
+                str_vac += str(vac)
+        return str_vac
+
+    def get_top_n_vacancy(self, top_n: int):
+        """Принимает число, возвращает список top-n вакансий."""
+        if len(self.__all_vacancies) <= top_n:
+            return self.__all_vacancies
+        top_n_vacs = Vacancies()
+        self.sort_vacancies_by_salary()
+        for vac in self.__all_vacancies[:top_n]:
+            top_n_vacs.add_vacancy(vac)
+
+        return top_n_vacs
+
+    def add_vacancy(self, new_vacancy):
+        """Добавляет вакансию к списку вакансий."""
+        if isinstance(new_vacancy, Vacancy):
+            self.__all_vacancies.append(new_vacancy)
+        else:
+            raise TypeError("Можно добавить только экземпляр класса Vacancy")
+
+    def delete_vacancy(self, vac_id: int):
+        """Удаляет вакансию из списка вакансий."""
+        new_vacancies = Vacancies()
+        for vac in self.__all_vacancies:
+            v = vac.to_dict()["id"]
+            if v.isdigit():
+                v = int(v)
             else:
-                continue
-        return cls.list_vacancies
+                return self.all_vacancies
+            if v != vac_id:
+                new_vacancies.add_vacancy(vac)
+        self.__all_vacancies = new_vacancies
+        return self.all_vacancies
+
+    def sort_vacancies_by_salary(self):
+        """Сортирует список вакансий по убыванию ЗП."""
+        self.__all_vacancies = sorted(self.__all_vacancies, reverse=True)
+
+    @property
+    def all_vacancies(self):
+        """Возвращает список вакансий."""
+        return self.__all_vacancies
+
+    def to_list_dict(self):
+        """Возвращает список словарей с вакансиями."""
+        my_list = []
+        for vac in self.__all_vacancies:
+            my_list.append(vac.to_dict())
+        return my_list
+
+    def filtered_by_city(self, city: str):
+        """Принимает название города, возвращает список вакансий в этом городе."""
+        by_city = Vacancies()
+        for vac in self.__all_vacancies:
+            if vac.to_dict()["city"].lower() == city:
+                by_city.add_vacancy(vac)
+        self.__all_vacancies = by_city
+        return self.__all_vacancies
+
+    def filtered_by_salary(self, salary_from: int):
+        """Принимает начальный размер ЗП, возвращает список вакансий ЗП в которых больше либо равны указанной сумме."""
+        by_salary = Vacancies()
+        for vac in self.__all_vacancies:
+            salary = vac.to_dict()["salary_from"]
+            if isinstance(salary, int) and (salary >= salary_from):
+                by_salary.add_vacancy(vac)
+        self.__all_vacancies = by_salary
+        return self.__all_vacancies
